f6bba87abe15a047f9fe55862ac832d9bf4694ce
Gustavo Franco <gufranco@users.noreply.github.com>
2023-05-19 13:45:38 -0300

    add gpg public key to rvm install

--- install.sh
+++ install.sh
@@ -401,6 +401,8 @@ ln -s ~/.dotfiles/git/.gitconfig ~/.gitconfig
 # RVM
 ################################################################################
 if [ -x "$(command -v rvm)" ]; then
+  gpg --keyserver keyserver.ubuntu.com --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
+
   curl -sSL https://get.rvm.io | sudo bash -s stable
 fi
 
